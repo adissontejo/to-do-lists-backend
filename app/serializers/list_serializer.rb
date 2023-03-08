1df641facc97168a7092a747f85b857666c88e66
adissontejo <adissonmprtejo@gmail.com>
2023-03-07 21:20:42 -0300

    Add items count

--- app/serializers/list_serializer.rb
+++ app/serializers/list_serializer.rb
@@ -4,7 +4,15 @@
 class ListSerializer
   # Short serializing
   class Short < ApplicationSerializer
-    attributes :id, :title, :description, :created_at, :updated_at
+    attributes :id, :title, :description, :created_at, :updated_at, :done_items, :total_items
+
+    def done_items
+      object.items.where(checked: true).count
+    end
+
+    def total_items
+      object.items.all.count
+    end
   end
 
   # Detailed serializing
